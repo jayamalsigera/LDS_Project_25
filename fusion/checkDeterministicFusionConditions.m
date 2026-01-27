ef69fe42c788feb69002969da4cf2b8b11d2f480
Mauricio Dada <mauricio@dada.eng.br>
2026-01-27 23:29:37 +0100

    Organize Scripts

--- fusion/checkDeterministicFusionConditions.m
+++ fusion/checkDeterministicFusionConditions.m
@@ -1,5 +1,5 @@
 %% Check if a node should transmit its data to the other nodes
-function c = checkDeterministicFusionCondition(X, x_bar, Omega, epsilon)
+function c = checkDeterministicFusionConditions(X, x_bar, Omega, epsilon)
   error = X - x_bar;
 
   % If error is larger than tolerance, then transmit
