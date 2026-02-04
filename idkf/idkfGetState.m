ca00df607b89a777ab5960471548b467e94bfd23
Mauricio Dada <mauricio@dada.eng.br>
2026-02-04 16:54:37 +0100

    Reorganize code into Matlab project (#3)
    * Reorganize StateSpaceModel class
    
    * Setup Matlab Project
    
    * Add .editorconfig file
    
    * Extract Kalman Filter functions
    
    * Extract Data Fusion Functions
    
    * Organize Scripts
    
    * Emphasize IDKF in functions
    
    * Mark matlab project files as linguist-generated
    
    * Include `idkf` in cSpell words

---
+++ idkf/idkfGetState.m
@@ -0,0 +1,7 @@
+%% Returning from information form to statespace-form state vectors
+function [X, x_bar] = idkfGetState(Omega, q, Omega_bar, q_bar)
+  % Modification of first equation in III B in:
+  % https://isas.iar.kit.edu/pdf/Fusion17_Pfaff-IDKF.pdf
+  X = Omega \ q;
+  x_bar = Omega_bar \ q_bar;
+end
