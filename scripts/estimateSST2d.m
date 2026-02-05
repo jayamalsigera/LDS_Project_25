75cb323b1f8f3710b2639b4d95c4c1a41bf9853e
Mauricio Dada <mauricio@dada.eng.br>
2026-02-05 01:39:43 +0100

    Create Random Spatial Network

--- scripts/estimateSST2d.m
+++ scripts/estimateSST2d.m
@@ -7,29 +7,37 @@ rng(42);
 steps = 100;
 x0 = [1 0]';
 
-plant = singleTargetTrackingPlant(Ts);
+nodeCount = 100;
+sensorCount = 20;
+maxLength = 5000;
 
-% Initialize vectors for storing states and outputs (include t=0).
-X = zeros(plant.n, steps + 1);
-Y = zeros(plant.p, steps + 1);
+% Distance-based spatial network
+[netGraph] = createSpatialNetwork(nodeCount, sensorCount, maxLength);
+plotNetwork(netGraph, maxLength);
 
-X(:,1) = x0;
-Y(:,1) = plant.outputEq(x0);
-for t = 2:steps + 1
-  [Y(:,t), X(:,t)] = plant.update(X(:, t-1));
-end
+% plant = singleTargetTrackingPlant(Ts);
 
-t = (0:steps) * Ts;
-figure
-plot(t, X');
-title("Simulated State of the Actual Model")
-xlabel('Time (s)');
+% % Initialize vectors for storing states and outputs (include t=0).
+% X = zeros(plant.n, steps + 1);
+% Y = zeros(plant.p, steps + 1);
 
-figure
-subplot(2, 1, 1)
-plot(t, Y(1:2,:)');
-title("Simulated Output of nodes 1 and 2")
-subplot(2, 1, 2)
-plot(t, Y(3:4,:)');
-title("Simulated Output of nodes 3 and 4")
-xlabel('Time (s)');
+% X(:,1) = x0;
+% Y(:,1) = plant.outputEq(x0);
+% for t = 2:steps + 1
+%   [Y(:,t), X(:,t)] = plant.update(X(:, t-1));
+% end
+
+% t = (0:steps) * Ts;
+% figure
+% plot(t, X');
+% title("Simulated State of the Actual Model")
+% xlabel('Time (s)');
+
+% figure
+% subplot(2, 1, 1)
+% plot(t, Y(1:2,:)');
+% title("Simulated Output of nodes 1 and 2")
+% subplot(2, 1, 2)
+% plot(t, Y(3:4,:)');
+% title("Simulated Output of nodes 3 and 4")
+% xlabel('Time (s)');
