b1ad495ec70750f40821d0f7d6174ea2c0f9ed05
Mauricio Dada <mauricio@dada.eng.br>
2026-02-09 22:22:51 +0100

    Fix issue with value class functions

--- estimators/DSEACP.m
+++ estimators/DSEACP.m
@@ -9,11 +9,6 @@ classdef DSEACP
     L
     N
     S
-    % Information Pair history
-    q_upd
-    q_pred
-    Omega_upd
-    Omega_pred
     % State estimate history
     x_hat
     % Model Matrices
@@ -45,38 +40,35 @@ classdef DSEACP
 
       self.n = plant.n;
 
-      self.q_upd = zeros(self.n, self.N, T + 1);
-      self.q_pred = zeros(self.n, self.N, T + 1);
-      self.Omega_upd = zeros(self.n, self.n, self.N, T + 1);
-      self.Omega_pred = zeros(self.n, self.n, self.N, T + 1);
-
       self.x_hat = zeros(self.n, self.N, T + 1);
     end
 
-    % TODO: This is failing because it's a value class. Need to return value instead of mutating
-    function update(self, t, y)
+    function [q_upd, Omega_upd] = update(self, q_pred, Omega_pred, y)
+      q_upd = zeros(self.n, self.N);
+      Omega_upd = zeros(self.n, self.n, self.N);
+
       for i = 1:self.N
         if self.G.Nodes(i, :).isSensor
           y_i = y(i:i + 1);
           C_i = self.C(i:i + 1, :);
-          R_i = self.R(i:i + 1, i:i + 1); % TODO: Review if this makes sense
+          R_i = self.R(i:i + 1, i:i + 1); % We can do this since D is block diagonal
 
-          self.q_upd(:, i, t) = self.q_pred(:, i, t - 1) + C_i' * (R_i \ y_i);
+          q_upd(:, i) = q_pred(:, i) + C_i' * (R_i \ y_i);
         else
-          self.q_upd(:, i, t) = self.q_pred(:, i, t - 1);
+          q_upd(:, i) = q_pred(:, i);
         end
 
-        self.Omega_upd(:, :, i, t) = self.Omega_upd(:, :, i, t - 1) + C_i' * (R_i \ C_i);
+        Omega_upd(:, :, i) = Omega_pred(:, :, i) + C_i' * (R_i \ C_i);
       end
     end
 
     %% Information Pair Fusion via Consensus Algorithm
-    function [q_fused, Omega_fused] = fusion(self, t)
+    function [q_fused, Omega_fused] = fusion(self, q_upd, Omega_upd)
       q_cons = zeros(self.n, self.N, self.L);
       Omega_cons = zeros(self.n, self.n, self.N, self.L);
 
-      q_cons(:, :, 1) = self.q_upd(:, :, t);
-      Omega_cons(:, :, :, 1) = self.Omega_upd(:, :, :, t);
+      q_cons(:, :, 1) = q_upd;
+      Omega_cons(:, :, :, 1) = Omega_upd;
 
       for l = 2:self.L
 
@@ -84,8 +76,8 @@ classdef DSEACP
           [~, nids] = inedges(self.G, i);
 
           for j = nids'
-            q_j = q_cons(:, j, t);
-            Omega_j = Omega_cons(:, :, j, t);
+            q_j = q_cons(:, j, l - 1);
+            Omega_j = Omega_cons(:, :, j, l - 1);
 
             w_ij = self.W(i, j);
 
@@ -101,48 +93,45 @@ classdef DSEACP
       Omega_fused = Omega_cons(:, :, :, end);
     end
 
-    function prediction(self, t, q_fused, Omega_fused)
+    function [q_pred, Omega_pred] = prediction(self, q_fused, Omega_fused)
+      q_pred = zeros(self.n, self.N);
+      Omega_pred = zeros(self.n, self.n, self.N);
+
       for i = 1:self.N
-        % TODO: Review/Cleanup
         q_i = q_fused(:, i);
         Omega_i = Omega_fused(:, :, i);
 
+        % TODO: Review/Cleanup
         I = eye(self.n);
-
         foo = inv(Omega_i + self.A' * (self.Q \ self.A));
-
         bar = self.A' \ (Omega_i * self.A');
-
         baz = self.A' \ Omega_i;
 
-        self.q_pred(:, i, t) = self.A' \ ((I - Omega_i * foo) * q_i);
-        self.Omega_pred(:, :, i, t) = bar - baz * foo * baz';
+        q_pred(:, i) = self.A' \ ((I - Omega_i * foo) * q_i);
+        Omega_pred(:, :, i) = bar - baz * foo * baz';
       end
 
     end
 
     function self = estimate(self, x0_hat, X, Y)
-      % Initializing from 0
-      % self.q_pred(:, :, 1) =
-      % self.Omega_pred(:, :, :, 1) =
+      % Initializing from 0 (no prior knowledge), since it's unrealistic to
+      % assume all nodes share same one. (Battistelli & Chisci, 2014)
+      q_pred = zeros(self.n, self.N);  % q_{0|-1}
+      Omega_pred = zeros(self.n, self.n, self.N);
+
       self.x_hat(:, :, 1) = repmat(x0_hat, 1, self.N);
 
       for t = 2:self.T + 1
         y = Y(:, t);
 
-        disp(self.q_upd(:, :, t))
-        self.update(t, y);
-        disp(self.q_upd(:, :, t))
-        [q_fused, Omega_fused] = self.fusion(t);
-        disp(self.q_upd(:, :, t))
-        self.prediction(t, q_fused, Omega_fused);
+        [q_upd, Omega_upd] = self.update(q_pred, Omega_pred, y);
+        [q_fused, Omega_fused] = self.fusion(q_upd, Omega_upd);
+        [q_pred, Omega_pred] = self.prediction(q_fused, Omega_fused);
 
-        % disp(self.Omega_upd(:, :, :, t))
-        % disp(self.q_upd(:, :, t))
+        disp(q_upd)
+        disp(Omega_upd)
 
-        % disp(self.Omega_upd)
-
-        self.x_hat(:, t) = self.Omega_upd(:, :, :, t) \ self.q_upd(:, :, t);
+        self.x_hat(:, t) = Omega_upd \ q_upd;
       end
 
       self.RMSE = vecnorm(self.x_hat - X);
