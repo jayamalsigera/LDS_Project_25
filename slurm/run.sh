e13a6754e9c7048999a03779b86a01c1c16082d7
Mauricio Dada <mauricio@dada.eng.br>
2026-07-20 11:47:20 -0300

    docs: include comment about memory usage for 32 CPUs

--- slurm/run.sh
+++ slurm/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #SBATCH --ntasks 1
 #SBATCH --partition allgroups
 #SBATCH --time 24:00:00
@@ -11,5 +10,7 @@
 #SBATCH --output results/slurm/%j-out.txt
 #SBATCH --error results/slurm/%j-err.txt
 #SBATCH --mail-type ALL
+
+# Use --mem=96G --cpus-per-task 32 for running the full 200 MC runs
 
 srun matlab -batch "$1"
